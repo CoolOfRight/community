defeb59de0820bc1d2251b8f98a64cb1656f1ee8
coolcoolofright <coolcoolofright@163.com>
2022-05-18 16:55:03 +0800

    2022.5.18.5：30十三次提交

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -49,7 +49,12 @@ public interface CommunityConstant {
      */
     String TOPIC_FOLLOW = "follow";
     /**
+     * 主题：发帖
+     */
+    String TOPIC_PUBLISH = "publish";
+    /**
      * 系统用户id
      */
     int SYSTEM_USER_ID = 1;
+
 }
