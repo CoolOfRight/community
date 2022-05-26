4c79766b95c947655705d028c22c951a8dd5b2e1
coolcoolofright <coolcoolofright@163.com>
2022-05-26 19:04:06 +0800

    2022.5.26.19:00 第十九次提交

--- src/main/resources/static/js/setting.js
+++ src/main/resources/static/js/setting.js
@@ -1,6 +1,7 @@
 $(function(){
     $("form").submit(check_data);
     $("input").focus(clear_error);
+    $("#uploadForm").submit(upload);
 });
 
 function check_data() {
@@ -15,4 +16,34 @@ function check_data() {
 
 function clear_error() {
     $(this).removeClass("is-invalid");
+}
+
+function upload() {
+    $.ajax({
+        url: "http://up-z2.qiniup.com",
+        method: "post",
+        processData: false,
+        contentType: false,
+        data: new FormData($("#uploadForm")[0]),
+        success: function(data){
+            if(data && data.code == 0){
+                //更新头像访问路径
+                $.post(
+                  CONTEXT_PATH + "/user/header/url",
+                    {"fileName":$("input[name='key']").val()},
+                    function(data){
+                      data = $.parseJSON(data);
+                      if(data.code == 0){
+                          window.location.reload();
+                      }else {
+                          alert(data.message);
+                      }
+                    }
+                );
+            }else {
+                alert("上传失败！")
+            }
+        }
+    });
+    return false;
 }
